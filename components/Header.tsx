8a2366e172504d712e846648ed44c4fa319db0c5
grvnt <109231551+grvnt@users.noreply.github.com>
2024-05-14 20:51:13 +0200

    First commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import ButtonSignin from "./ButtonSignin";
-import logo from "@/app/icon.png";
+import logo from "@/app/explosivex2-logo.png";
 import config from "@/config";
 
 const links: {
